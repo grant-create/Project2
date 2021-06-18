f9e5e6df8afe991f303eeaf933c8bae38de00e18
grant-create <grant.s.wilkie@gmail.com>
2021-06-18 11:39:18 -0700

    seeding

--- testStock.js
+++ testStock.js
@@ -24,8 +24,8 @@ const alpaca = new Alpaca({keyId:alpApiKey, secretKey:alpSecretKey, paper: true,
 
 
 // SHORTER LIST
-// listOfStocks = ['SPY', 'QQQ', 'DIA', 'MMM', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADBE', 'AMD', 'AAP', 'AES', 'AFL', 'A','LUV', 'SYF', 'SYY', 'TPR', 'TXT', 'TWTR', 'TSN', 'UDR', 'USB', 'UAA', 'UA', 'UAL', 'UNM']
- listOfStocks = ['SPY', 'QQQ', 'DIA']
+listOfStocks = ['SPY', 'QQQ', 'DIA', 'MMM', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADBE', 'AMD', 'AAP', 'AES', 'AFL', 'A','LUV', 'SYF', 'SYY', 'TPR', 'TXT', 'TWTR', 'TSN', 'UDR', 'USB', 'UAA', 'UA', 'UAL', 'UNM']
+//  listOfStocks = ['SPY', 'QQQ', 'DIA']
 
 
   async function getInfo(){
@@ -126,12 +126,12 @@ const alpaca = new Alpaca({keyId:alpApiKey, secretKey:alpSecretKey, paper: true,
 // db.stocks.sync({ force: true})
 // db.details.sync({ force: true}).then(()=>process.exit())
 // db.wsb.sync({ force: true}).then(()=>process.exit)
-db.sequelize.sync({ force: true}).then(()=>process.exit())
+// db.sequelize.sync({ force: true}).then(()=>process.exit())
 
 
 
 
-// getInfo()
+getInfo()
 
 
 
