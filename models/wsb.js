cde2e3d57cd8aabbb2829301bb3880d8c7ae70f2
grant-create <grant.s.wilkie@gmail.com>
2021-06-18 11:36:17 -0700

    test again

--- models/wsb.js
+++ models/wsb.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // models.wsb.belongsTo(models.stocks, {foreignKey: 'ticker', targetKey: 'ticker'})
+    
     }
   };
   wsb.init({
