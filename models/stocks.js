1552e23d37a89325e5bfc01564a496273e5c9522
grant-create <grant.s.wilkie@gmail.com>
2021-06-18 11:34:37 -0700

    test again

--- models/stocks.js
+++ models/stocks.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.stocks.hasOne(models.wsb, {foreignKey: 'ticker', targetKey: 'ticker'})
+      // models.stocks.hasOne(models.wsb, {foreignKey: 'ticker', targetKey: 'ticker'})
       models.stocks.hasOne(models.details)
     }
   };
